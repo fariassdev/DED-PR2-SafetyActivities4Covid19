0ed2667500019a49bd7c73ae1b7f4311883537df
Fernando Arias <fernan.fpinformatica@gmail.com>
2022-01-13 00:19:58 +0100

    add organization and role to Worker constructor

--- src/main/java/uoc/ded/practica/model/Worker.java
+++ src/main/java/uoc/ded/practica/model/Worker.java
@@ -5,9 +5,12 @@ import java.time.LocalDate;
 public class Worker extends User {
 
     private Role role;
+    private Organization organization;
 
-	public Worker(String idUser, String name, String surname, LocalDate birthday, boolean covidCertificate) {
+	public Worker(String idUser, String name, String surname, LocalDate birthday, boolean covidCertificate, Role role, Organization organization) {
         super(idUser, name, surname, birthday, covidCertificate);
+        this.setRole(role);
+        this.setOrganization(organization);
     }
 
     public String getRoleId() {
@@ -20,5 +23,13 @@ public class Worker extends User {
 
     public void setRole(Role role) {
         this.role = role;
+    }
+
+    public Organization getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(Organization organization) {
+        this.organization = organization;
     }
 }
