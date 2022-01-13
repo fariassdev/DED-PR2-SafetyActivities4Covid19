69f23b8baba4a9a9217e1305cef4dace5aba96c5
Fernando Arias <fernan.fpinformatica@gmail.com>
2022-01-13 18:41:16 +0100

    add remaining attributes and methods to Order model

--- src/main/java/uoc/ded/practica/model/Order.java
+++ src/main/java/uoc/ded/practica/model/Order.java
@@ -1,14 +1,71 @@
 package uoc.ded.practica.model;
 
+import uoc.ei.tads.Cola;
+import uoc.ei.tads.ColaConPrioridad;
+import uoc.ei.tads.Iterador;
+
 public class Order {
+    private String orderId;
     private User user;
     private Activity activity;
+    private ColaConPrioridad<Ticket> tickets;
     private int seat;
+    private int nextSeat;
 
 
-    public Order(User user, Activity activity) {
+    public Order(String orderId, User user, Activity activity, ColaConPrioridad<Ticket> tickets, int seat) {
+        this.orderId = orderId;
         this.user = user;
         this.activity = activity;
+        this.tickets = tickets;
+        this.seat = seat;
+        this.nextSeat = 1;
+    }
+
+    public int getValue() {
+        return 0;
+    }
+
+    public String getId() {
+        return this.orderId;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Activity getActivity() {
+        return activity;
+    }
+
+    public void setActivity(Activity activity) {
+        this.activity = activity;
+    }
+
+    public void addTicket(User user) {
+        tickets.encolar(new Ticket(user, this.activity));
+    }
+
+    public Ticket pop() {
+        Ticket t = tickets.desencolar();
+        t.setSeat(nextSeat++);
+        return t;
+    }
+
+    public Iterador<Ticket> tickets() {
+        return tickets.elementos();
     }
 
     public int getSeat() {
@@ -17,18 +74,5 @@ public class Order {
 
     public void setSeat(int seat) {
         this.seat = seat;
-    }
-
-    @Override
-    public String toString() {
-        return "**" + activity.getActId() + " " + seat + " " + user.getId();
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public Activity getActivity() {
-        return activity;
     }
 }
