afee2a27dc7a56f9d963c6e6100ea85a2f87b4f0
Fernando Arias <fernan.fpinformatica@gmail.com>
2022-01-14 04:56:35 +0100

    add workers LinkedList to Role model

--- src/main/java/uoc/ded/practica/model/Role.java
+++ src/main/java/uoc/ded/practica/model/Role.java
@@ -1,12 +1,19 @@
 package uoc.ded.practica.model;
 
+import uoc.ei.tads.Iterador;
+import uoc.ei.tads.ListaEncadenada;
+import uoc.ei.tads.Posicion;
+import uoc.ei.tads.Recorrido;
+
 public class Role {
     private String roleId;
     private String name;
+    private ListaEncadenada<Worker> workers;
 
     public Role(String roleId, String name) {
         this.roleId = roleId;
         this.name = name;
+        workers = new ListaEncadenada<Worker>();
     }
 
     public String getRoleId() {
@@ -27,5 +34,36 @@ public class Role {
 
     public boolean is(String roleId) {
         return this.roleId.equals(roleId);
+    }
+
+    public Iterador<Worker> workers() {
+        return workers.elementos();
+    }
+
+    public Recorrido<Worker> workerPositions() {
+        return workers.posiciones();
+    }
+
+    public int numWorkers() {
+        return workers.numElems();
+    }
+
+    public boolean hasWorkers() {
+        return workers.numElems() > 0;
+    }
+
+    public void addWorker( Worker user) {
+        workers.insertarAlFinal(user);
+    }
+
+    public void deleteWorker( String workerId ) {
+        for ( Recorrido<Worker> it = this.workerPositions(); it.haySiguiente(); ) {
+            final Posicion<Worker> workerPosition = it.siguiente();
+            Worker worker = workerPosition.getElem();
+            if ( worker.getId().equals( workerId ) ) {
+                workers.borrar( workerPosition );
+            }
+
+        }
     }
 }
